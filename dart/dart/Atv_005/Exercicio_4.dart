64418875de77cafdfceae5eea4fd9ed0a5e4662a
FredericoVianna <fredyvianna137@gmail.com>
2023-05-09 21:36:32 -0300

    09/05

--- dart/dart/Atv_005/Exercicio_4.dart
+++ dart/dart/Atv_005/Exercicio_4.dart
@@ -1,10 +1,8 @@
 
 
 
-//conversor de kilos pra litros
+//medidor de areas
 
 //exercicio 4
-//mostra para o usuario um cabeçalho com 3 opições de conversão diferentes 
-//gramas em mls litros pra kilos e toneladas pra metro cubico em seguida
-//ele adiciona o resultado em uma lista de resultado de conversoes de medidas
-//depois usa uma função anonima pra varrer a lista e printar para o usuario
+//usuario escolhe entre medir a are de 4 formas geometricas 
+//octogono, pentagono, prisma e triangulo.
