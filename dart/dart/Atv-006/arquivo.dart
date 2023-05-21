aa2cb87ffb86747e54448b6f475aef64177cf1a1
FredyZ-c137 <fredyvianna137@gmail.com>
2023-05-21 13:31:19 -0300

    21/05

---
+++ dart/dart/Atv-006/arquivo.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main() {
+  while (true) {
+    final caminho = File('../arquivo/cadastro_json.txt');
+    final listaMapas = lerMapasDoArquivo(caminho);
+
+    exibirMenu();
+    final opcao = stdin.readLineSync()!.toLowerCase();
+
+    switch (opcao) {
+      case '1':
+        print('\nConteúdo do arquivo:');
+        listaMapas.forEach((mapa) => print(json.encode(mapa)));
+        break;
+      case '2':
+        stdout.write('\nDigite o número do mapa que deseja excluir: ');
+        final linhaParaExcluir = int.parse(stdin.readLineSync()!);
+
+        final listaAtualizada = excluirLinha(listaMapas, linhaParaExcluir);
+
+        if (listaAtualizada != null) {
+          final novoConteudo =
+              listaAtualizada.map((mapa) => json.encode(mapa)).join('\n');
+          caminho.writeAsStringSync(novoConteudo);
+          print('Linha $linhaParaExcluir excluída com sucesso!');
+        } else {
+          print('Falha ao excluir a linha $linhaParaExcluir.');
+        }
+        break;
+      case '3':
+        final mapa = obterInformacoesDoUsuario(listaMapas.length + 1);
+
+        try {
+          final mapaCodificado = json.encode(mapa);
+          caminho.writeAsStringSync('\n$mapaCodificado\n', mode: FileMode.append);
+          print('Mapa adicionado com sucesso!');
+        } catch (e) {
+          print('Ocorreu um erro ao adicionar o mapa: $e');
+        }
+        break;
+
+      case '4':
+        alteracoes(listaMapas);
+        print('Novo mapa adicionado com sucesso!');
+      break;  
+      case 'sair':
+        print('Encerrando o programa...');
+        return;
+      default:
+        print('Opção inválida. Tente novamente.');
+        break;
+    }
+  }
+}
+
+void exibirMenu() {
+  stdout.write('''
+Escolha o que deseja fazer:
+
+1 - Ler o arquivo
+2 - Excluir um elemento do arquivo
+3 - Adicionar um elemento no arquivo
+4 - Alterar conteudo do arquivo
+'Sair' - para encerrar o programa
+
+''');
+}
+
+List<Map<String, dynamic>> lerMapasDoArquivo(File arquivo) {
+  final conteudo = arquivo.readAsLinesSync();
+  final listaMapas = [];
+
+  for (var i = 0; i < conteudo.length; i++) {
+    final linha = conteudo[i].trim();
+
+    if (linha.isNotEmpty) {
+      try {
+        final mapaDecodificado = json.decode(linha);
+        listaMapas.add(mapaDecodificado);
+      } catch (e) {
+        print('Ocorreu um erro ao decodificar o mapa ${i + 1}: $e');
+      }
+    }
+  }
+
+  return listaMapas.cast<Map<String, dynamic>>();
+}
+
+List<dynamic>? excluirLinha(List<dynamic> lista, int linhaParaExcluir) {
+  if (linhaParaExcluir > 0 && linhaParaExcluir <= lista.length) {
+    lista.removeAt(linhaParaExcluir - 1);
+    return lista;
+  }
+  return null;
+}
+
+Map<String, dynamic> obterInformacoesDoUsuario(int numeroMapa) {
+  print('=== Preenchendo o Mapa $numeroMapa ===');
+  stdout.write('Digite o nome: ');
+  final nome = stdin.readLineSync()!;
+
+  stdout.write('Digite a idade: ');
+  final idade = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Digite a cidade: ');
+  final cidade = stdin.readLineSync()!;
+
+  return {'nome': nome, 'idade': idade, 'cidade': cidade};
+}
+
+void alteracoes(List<Map<String, dynamic>> arquivos) {
+  print('Digite o nome que deseja modificar: ');
+  String? nome = stdin.readLineSync();
+  
+  // Procura o mapa na lista com base no nome
+  int index = -1;
+  for (int i = 0; i < arquivos.length; i++) {
+    if (arquivos[i]['nome'] == nome) {
+      index = i;
+      break;
+    }
+  }
+  
+  if (index != -1) {
+    print('Digite o novo nome: ');
+    String? novoNome = stdin.readLineSync();
+    
+    // Atualiza o nome do mapa encontrado
+    arquivos[index]['nome'] = novoNome;
+    print(arquivos);
+
+    // Grava as alterações no arquivo
+    final caminho = File('../arquivo/cadastro_json.txt');
+    caminho.writeAsStringSync('');
+    arquivos.forEach((mapa) {
+      final mapaCodificado = json.encode(mapa);
+      caminho.writeAsStringSync('$mapaCodificado\n', mode: FileMode.append);
+    });
+
+    print('Arquivo atualizado com sucesso!');
+  } else {
+    print('Mapa não encontrado.');
+  }
+}
--- dart/dart/Atv-006/exercicio_1.dart
+++ dart/dart/Atv-006/exercicio_1.dart
@@ -2,138 +2,67 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-  //declarando uma lista vazia de resultados
-  List<dynamic> resultados = [];
+  Map<String, int> listaTelefonica = {};
 
-  //Loop infinito para o usuário poder realizar várias operações
   while (true) {
-    //Exibe o menu para o usuário
-    exibirMenu();
+    print('-' * 70);
+    print('EXERCÍCIO C: WHILE, SWITCH IF');
+    print('-' * 70);
+    print('Escolha uma opção:');
+    print('1 - Inserir nome e telefone');
+    print('2 - Exibir lista nomes e telefones');
+    print('3 - Sair do programa');
 
-    //Recebe a opção escolhida pelo usuário
-    String? opcao = stdin.readLineSync();
+    int num = int.parse(stdin.readLineSync()!);
 
-    //Verifica qual operação o usuário escolheu
-    switch (opcao) {
-      case '1':
-        realizarOperacao(somar, resultados);
+    switch (num) {
+      case 1:
+        stdout.write('Digite um nome: ');
+        String nome1 = stdin.readLineSync()!;
+        stdout.write('Digite um número de telefone: ');
+        int telefone = int.parse(stdin.readLineSync()!);
+
+        if (nome1.isNotEmpty && telefone != null) {
+          listaTelefonica.putIfAbsent(nome1, () => telefone);
+        }
+        print('Pressione Enter para continuar ou digite "sair" para sair do programa!');
+        String opcao = stdin.readLineSync()!;
+        if (opcao == 'sair') {
+          print('FIM DO PROGRAMA!');
+          adicionar(listaTelefonica);
+          return;
+        }
         break;
-      case '2':
-        realizarOperacao(subtrair, resultados);
+
+      case 2:
+        listaTelefonica.forEach((key, value) {
+          print('$key: $value');
+        });
+        print('Pressione Enter para continuar ou digite "sair" para sair do programa!');
+        String opcao = stdin.readLineSync()!;
+        if (opcao == 'sair') {
+          print('FIM DO PROGRAMA!');
+          return;
+        }
         break;
-      case '3':
-        realizarOperacao(multiplicar, resultados);
-        break;
-      case '4':
-        realizarOperacao(dividir, resultados);
-        break;
-      case '5':
-        print('Fim do programa!');
-        varreduraComMensagem(resultados, mensagem: 'Historico de resultados: ');
+
+      case 3:
+        print('FIM DO PROGRAMA!');
+        adicionar(listaTelefonica);
         return;
-      default:
-        print('Opção inválida, tente novamente.');
-        break;
     }
   }
 }
 
-//Exibe o menu para o usuário
-void exibirMenu() {
-  print('Escolha uma das operações abaixo:');
-  print('1 - Somar');
-  print('2 - Subtrair');
-  print('3 - Multiplicar');
-  print('4 - Dividir');
-  print('5 - Sair');
-}
+void adicionar(Map<String, int> listaTelefonica) {
+  final arquivo = File('../arquivo/listaTelefonica_json1.txt');
 
-//Realiza a operação escolhida pelo usuário
-void realizarOperacao(Function operacao, List<dynamic> resultados) {
-  //Recebe os valores para a operação 
-  stdout.write('Digite o primeiro número: ');
-  double? valor1 = double.tryParse(stdin.readLineSync()!);
-  stdout.write('Digite o segundo número: ');
-  double? valor2 = double.tryParse(stdin.readLineSync()!);
-
-  //Verifica se os valores foram recebidos corretamente
-  if (valor1 == null || valor2 == null) {
-    print('Valores inválidos, tente novamente.');
-    return;
-  }
-
-  final arquivo = File('../arquivo/teste_json.txt'); 
-
-  lerMapasDoArquivo(arquivo);
-
-  //Realiza a operação e exibe o resultado
-  dynamic resultado = operacao(valor1, valor2);
-  resultados.add(resultado);
-  print('Resultado: $resultado');
-
-  if(resultados != null){
-
-      dynamic mapa = resultados;
-
-      final mapaCodificado = json.encode(mapa);
-      //FileMode.append para gravar
-      arquivo.writeAsStringSync('\n' + mapaCodificado + '\n', mode: FileMode.append);
-      print('Calculo gravado com sucesso!');
-    } else {
-      print('Ocorreu um erro ao gravar o resultado');
-    }
-
-}
-
-//Funções matemáticas
-double somar(double valor1, double valor2) {
-  return valor1 + valor2;
-}
-
-double subtrair(double valor1, double valor2) {
-  return valor1 - valor2;
-}
-
-double multiplicar(double valor1, double valor2) {
-  return valor1 * valor2;
-}
-
-double dividir(double valor1, double valor2) {
-  return valor1 / valor2;
-}
-
-//função anonima que varre a lista e exibi os elementos 
-void varreduraComMensagem(List<dynamic> resultados, {String? mensagem}) {
-  //condicional que confere se a variavel mensagem e nula
-  if (mensagem != null) {
-    print(mensagem);
-  }
-  //Uma função anônima que imprime cada elemento da lista
-  resultados.forEach((elemento) => print(elemento));
-}
-
-void lerMapasDoArquivo(File arquivo) {
-  final linhas = arquivo.readAsLinesSync();
-
-  for (var i = 0; i < linhas.length; i++) {
-    final mapaDecodificado = json.decode(linhas[i]);
-    print('\n=== calculo ${i + 1} ===');
-    printMap(mapaDecodificado);
+  if (listaTelefonica.isNotEmpty) {
+    dynamic mapa = listaTelefonica;
+    final mapaCodificado = json.encode(mapa);
+    arquivo.writeAsStringSync(mapaCodificado + '\n', mode: FileMode.append);
+    print('Cálculo gravado com sucesso!');
+  } else {
+    print('Ocorreu um erro ao gravar o resultado');
   }
 }
-
-void printMap(Map<String, dynamic> mapa){
-  mapa.forEach((key, value) {
-    print('$key: $value');
-  });
-}
-
-//Funcionalidade
-
-//Esse código Dart é um simples programa de calculadora que permite ao usuário 
-//realizar operações matemáticas básicas (adição, subtração, multiplicação e divisão) 
-//em um loop infinito. Ele exibe um menu de opções para o usuário escolher a operação desejada, 
-//lê os números de entrada do usuário e realiza a operação escolhida, exibindo o resultado. 
-//Ele também mantém um histórico de resultados e permite que o usuário visualize o histórico 
-//antes de encerrar o programa. As funções matemáticas são definidas separadamente como funções 
-//que recebem dois números e retornam o resultado da operação correspondente. 
