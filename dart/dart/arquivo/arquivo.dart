1fd8afa27eaf326745afb39dbb7612b39ffa9c0c
FredyZ-c137 <fredyvianna137@gmail.com>
2023-05-21 12:41:30 -0300

    21/05

--- dart/dart/arquivo/arquivo.dart
+++ dart/dart/arquivo/arquivo.dart
@@ -2,46 +2,80 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-   final caminho = File('../arquivo/teste_json.txt');
+  while (true) {
+    final caminho = File('../arquivo/teste_json.txt');
+    final listaMapas = lerMapasDoArquivo(caminho);
 
-  final listaMapas = lerMapasDoArquivo(caminho);
+    exibirMenu();
+    final opcao = stdin.readLineSync()!.toLowerCase();
 
-  stdout.write('\nDigite o número do mapa que deseja modificar: ');
-  final linhaParaExcluir = int.parse(stdin.readLineSync()!);
+    switch (opcao) {
+      case '1':
+        print('\nConteúdo do arquivo:');
+        listaMapas.forEach((mapa) => print(json.encode(mapa)));
+        break;
+      case '2':
+        stdout.write('\nDigite o número do mapa que deseja excluir: ');
+        final linhaParaExcluir = int.parse(stdin.readLineSync()!);
 
-  final listaAtualizada = excluirLinha(listaMapas, linhaParaExcluir);
+        final listaAtualizada = excluirLinha(listaMapas, linhaParaExcluir);
 
-  if(listaAtualizada != null){
-    /*
-    */
-    final novoConteudo = 
-    listaAtualizada.map((mapa) => json.encode(mapa)).join('\n');
-    caminho.writeAsStringSync(novoConteudo);
-    print('Linha ${linhaParaExcluir} selecionada com sucesso!'); 
-  } else {
-    print('Falha ao modificar a linha ${linhaParaExcluir}.');
-  }
-  final mapa = obterInformacoesDoUsuario(linhaParaExcluir);
+        if (listaAtualizada != null) {
+          final novoConteudo =
+              listaAtualizada.map((mapa) => json.encode(mapa)).join('\n');
+          caminho.writeAsStringSync(novoConteudo);
+          print('Linha $linhaParaExcluir excluída com sucesso!');
+        } else {
+          print('Falha ao excluir a linha $linhaParaExcluir.');
+        }
+        break;
+      case '3':
+        final mapa = obterInformacoesDoUsuario(listaMapas.length + 1);
 
-   try {
-      int i = linhaParaExcluir;
-      final mapaCodificado = json.encode(mapa);
-      //FileMode.append para gravar
-      caminho.writeAsStringSync('\n'+mapaCodificado + '\n', mode: FileMode.append);
-      print('Mapa $i alterado com sucesso!');
-    } catch (e) {
-      print('Ocorreu um erro ao alterar o mapa: $e');
+        try {
+          final mapaCodificado = json.encode(mapa);
+          caminho.writeAsStringSync('\n$mapaCodificado\n', mode: FileMode.append);
+          print('Mapa adicionado com sucesso!');
+        } catch (e) {
+          print('Ocorreu um erro ao adicionar o mapa: $e');
+        }
+        break;
+
+      case '4':
+        alteracoes(listaMapas);
+        print('Novo mapa adicionado com sucesso!');
+      break;  
+      case 'sair':
+        print('Encerrando o programa...');
+        return;
+      default:
+        print('Opção inválida. Tente novamente.');
+        break;
     }
+  }
+}
+
+void exibirMenu() {
+  stdout.write('''
+Escolha o que deseja fazer:
+
+1 - Ler o arquivo
+2 - Excluir um elemento do arquivo
+3 - Adicionar um elemento no arquivo
+4 - Alterar conteudo do arquivo
+'Sair' - para encerrar o programa
+
+''');
 }
 
 List<Map<String, dynamic>> lerMapasDoArquivo(File arquivo) {
   final conteudo = arquivo.readAsLinesSync();
   final listaMapas = [];
 
-  for (var i = 0; i< conteudo.length; i++) {
+  for (var i = 0; i < conteudo.length; i++) {
     final linha = conteudo[i].trim();
 
-    if(linha.isNotEmpty) {
+    if (linha.isNotEmpty) {
       try {
         final mapaDecodificado = json.decode(linha);
         listaMapas.add(mapaDecodificado);
@@ -50,9 +84,7 @@ List<Map<String, dynamic>> lerMapasDoArquivo(File arquivo) {
       }
     }
   }
-  /*
-  
-  */
+
   return listaMapas.cast<Map<String, dynamic>>();
 }
 
@@ -65,8 +97,8 @@ List<dynamic>? excluirLinha(List<dynamic> lista, int linhaParaExcluir) {
 }
 
 Map<String, dynamic> obterInformacoesDoUsuario(int numeroMapa) {
-  print('=== Preencheado o Mapa $numeroMapa ===');
-  stdout.write('Digite o nome: '); 
+  print('=== Preenchendo o Mapa $numeroMapa ===');
+  stdout.write('Digite o nome: ');
   final nome = stdin.readLineSync()!;
 
   stdout.write('Digite a idade: ');
@@ -75,5 +107,40 @@ Map<String, dynamic> obterInformacoesDoUsuario(int numeroMapa) {
   stdout.write('Digite a cidade: ');
   final cidade = stdin.readLineSync()!;
 
-  return {' nome': nome, 'idade': idade, 'cidade': cidade};
+  return {'nome': nome, 'idade': idade, 'cidade': cidade};
+}
+
+void alteracoes(List<Map<String, dynamic>> arquivos) {
+  print('Digite o nome que deseja modificar: ');
+  String? nome = stdin.readLineSync();
+  
+  // Procura o mapa na lista com base no nome
+  int index = -1;
+  for (int i = 0; i < arquivos.length; i++) {
+    if (arquivos[i]['nome'] == nome) {
+      index = i;
+      break;
+    }
+  }
+  
+  if (index != -1) {
+    print('Digite o novo nome: ');
+    String? novoNome = stdin.readLineSync();
+    
+    // Atualiza o nome do mapa encontrado
+    arquivos[index]['nome'] = novoNome;
+    print(arquivos);
+
+    // Grava as alterações no arquivo
+    final caminho = File('../arquivo/teste_json.txt');
+    caminho.writeAsStringSync('');
+    arquivos.forEach((mapa) {
+      final mapaCodificado = json.encode(mapa);
+      caminho.writeAsStringSync('$mapaCodificado\n', mode: FileMode.append);
+    });
+
+    print('Arquivo atualizado com sucesso!');
+  } else {
+    print('Mapa não encontrado.');
+  }
 }
